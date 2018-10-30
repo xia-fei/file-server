c968af93b9656e7502607de4b0d27532ef9f72a6
夏飞 <xiafei@qccr.com>
2018-10-30 15:36:06 +0800

    init

---
+++ src/main/java/org/xiafei/file/UploadController.java
@@ -0,0 +1,52 @@
+package org.xiafei.file;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.PutObjectResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+/**
+ * @author 夏飞
+ */
+
+@RestController
+public class UploadController {
+    @RequestMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) throws IOException {
+        String  filePath=generateFilePath(file.getOriginalFilename());
+        uploadAliOSS(file.getInputStream(), filePath);
+        return "https://xiafei-files.oss-cn-hangzhou.aliyuncs.com/" + filePath;
+    }
+
+    private void uploadAliOSS(InputStream inputStream, String key) throws FileNotFoundException {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+        String accessKeyId = "LTAIX3Mkmhdk2GeB";
+        String accessKeySecret = "QGWGUdG8wxbNTz1QCQ05SbNifXXZ7n";
+
+// 创建OSSClient实例。
+        OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
+
+// 上传文件流。
+        PutObjectResult putObjectResult = ossClient.putObject("xiafei-files", key, inputStream);
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
+
+    /**
+     * 生成文件目录
+     * /file/Timestamp
+     */
+    private String generateFilePath(String originFileName) {
+        return System.currentTimeMillis() + getFileNameSuffix(originFileName);
+    }
+
+    private String getFileNameSuffix(String fileName) {
+        return fileName.substring(fileName.indexOf("."));
+    }
+}
